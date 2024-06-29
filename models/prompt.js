882a261ed8ae261ab7e1dffad0685fa295a6e26e
pixelrakete <info@pixelrakete.de>
2024-06-29 12:45:31 +0200

    Update prompt.js

--- models/prompt.js
+++ models/prompt.js
@@ -1,1 +1,14 @@
- 
+ const mongoose = require('mongoose');
+
+const promptSchema = new mongoose.Schema({
+  name: String,
+  text: String,
+  folder: String,
+  color: String,
+  variables: [String],
+  final: Boolean,
+  version: Number,
+  shareId: String
+});
+
+module.exports = mongoose.model('Prompt', promptSchema);
