21d41e09a559628fea2a031da3c9eb589a41d3d3
pixelrakete <info@pixelrakete.de>
2024-06-30 14:22:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ app.use(bodyParser.json());
 
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(() => {
   console.log('Connected to MongoDB Atlas');
 })
